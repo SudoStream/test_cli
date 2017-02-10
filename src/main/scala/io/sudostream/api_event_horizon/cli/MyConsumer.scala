beb2c19f45726a9897bbfe906d4f8df29e5c5cdb
Andy Boyle <andy@sudostream.io>
2017-02-10 14:36:03 +0000

    minor renaming

--- src/main/scala/io/sudostream/api_event_horizon/cli/MyConsumer.scala
+++ src/main/scala/io/sudostream/api_event_horizon/cli/MyConsumer.scala
@@ -7,7 +7,7 @@ import akka.kafka.{ConsumerSettings, Subscriptions}
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Sink
 import com.typesafe.config.ConfigFactory
-import io.sudostream.api_event_horizon.kafka.serialising.SpeculativeScreenPlayDeserialiser
+import io.sudostream.api_event_horizon.kafka.serialising.SpeculativeScreenplayDeserialiser
 import org.apache.kafka.clients.consumer.ConsumerConfig
 import org.apache.kafka.common.serialization.ByteArrayDeserializer
 
@@ -22,14 +22,14 @@ object MyConsumer {
   val config = ConfigFactory.load()
   val logger = Logging(system, getClass)
 
-  val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new SpeculativeScreenPlayDeserialiser)
+  val consumerSettings = ConsumerSettings(system, new ByteArrayDeserializer, new SpeculativeScreenplayDeserialiser)
     .withBootstrapServers("localhost:9092")
     .withGroupId("aeh-cli-listener")
     .withProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest")
 
   def main(args: Array[String]) {
     val done =
-      Consumer.committableSource(consumerSettings, Subscriptions.topics("generated-test-scripts"))
+      Consumer.committableSource(consumerSettings, Subscriptions.topics("speculative-screenplays"))
         .map {
           msg =>
             println("Test Script = " + msg.record.value())
